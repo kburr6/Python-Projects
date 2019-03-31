4947bf29baac16f6af9610545a7983c575163e09
kburr6 <48641450+kburr6@users.noreply.github.com>
2019-03-31 00:57:58 -0500

    sample code

---
+++ Strings and Lists/convert_to_all_caps.py
@@ -0,0 +1,5 @@
+# Example input() statement
+s = input('Please enter a string: ')
+s = s.upper()
+# Print the string when you are done
+print(s)
---
+++ Strings and Lists/string_formatting_examples.py
@@ -0,0 +1,19 @@
+# Example input() statement
+s = input('Please enter a string: ')
+
+# Print out a friendly greeting in response
+print('Hello, {}!'.format(s))
+
+# Example input() statement
+n = int(input('Please enter an integer: '))
+
+# Place your print statement here
+print('Two times {} is {}.'.format(n,2*n))
+
+from math import pi
+
+# Example input() statement
+n = int(input('Please enter an integer: '))
+
+# Replace this with your own print statement
+print('{:.{}f}'.format(pi,n))
